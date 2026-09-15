d6d2fa84ba63970ac36d726137c1aee6da4559d4
Стародубцев Максим Алексеевич <106010218@ACADEMY.TOP>
2026-09-15 12:56:35 +0300

    Initial commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetWebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public string Name { get; set; }
+
+    [BindProperty]
+    public string Phone { get; set; }
+
+    [BindProperty]
+    public string Email { get; set; }
+
+    [BindProperty]
+    public string Speciality { get; set; }
+
+    [BindProperty]
+    public string Course { get; set; }
+
+    [BindProperty]
+    public string BirthDate { get; set; }
+
+    [BindProperty]
+    public string[] Technologies { get; set; } = Array.Empty<string>();
+
+  
+    [BindProperty]
+    public string City { get; set; } 
+
+    [BindProperty]
+    public string MainLanguage { get; set; } 
+
+    [BindProperty]
+    public string EducationFormat { get; set; } 
+
+    [BindProperty]
+    public string AboutSelf { get; set; }
+
+    public string Message { get; set; }
+
+    public void OnGet()
+    {
+        // Message = "Привет! Сообщение от C#";
+    }
+
+    public IActionResult OnPost()
+    {
+        string technologies = Technologies.Length > 0 ? string.Join(", ", Technologies) : "Не выбраны";
+
+       
+        string message = $"Анкета студента\n\n" +
+                         $"Имя: {Name}\n" +
+                         $"Телефон: {Phone}\n" +
+                         $"Email: {Email}\n" +
+                         $"Дата рождения: {BirthDate}\n" +
+                         $"Город: {City}\n" +
+                         $"Специальность: {Speciality}\n" +
+                         $"Курс: {Course}\n" +
+                         $"Основной язык: {MainLanguage}\n" + 
+                         $"Формат обучения: {EducationFormat}\n" + 
+                         $"Технологии: {technologies}\n" +
+                         $"О себе: {AboutSelf}"; 
+
+        return Content(message);
+    }
+}
